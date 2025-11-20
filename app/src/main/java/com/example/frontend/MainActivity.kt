27b5e652f8bf0283b0d59c2866cfa2c0faa15652
erick <molinaerick222@gmail.com>
2025-11-19 22:15:01 -0600

    "Avance login con pantalla de carga"

--- app/src/main/java/com/example/frontend/MainActivity.kt
+++ app/src/main/java/com/example/frontend/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.frontend
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,12 +14,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        // Configuración visual para pantalla completa
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // --- AQUÍ EMPIEZA LA MAGIA ---
+
+        // Esperar 3 segundos (3000 milisegundos)
+        Handler(Looper.getMainLooper()).postDelayed({
+
+            // 1. Crear el viaje hacia CategoriesActivity
+            val intent = Intent(this, CategoriesActivity::class.java)
+            startActivity(intent)
+
+            // 2. Cerrar esta pantalla de carga para que no se pueda volver a ella
+            finish()
+
+        }, 3000)
     }
 }
-//Primer commit //
-//segundo commit//
